090a99be5025fe9a8567605cf076d9c7ff3ad077
Vaiva Grigaite <vaiva.grigaite@ktu.edu>
2019-11-11 19:21:16 +0000

    du kintamieji

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello, everyone!")
+import sys
+
+def greet(name:str, shout_count:int=1):
+    return f"Hello, {name}{shout_count*'!'}"
+    
+if len(sys.argv) == 3 :
+    n = int(sys.argv[2])
+elif len(sys.argv) == 2:
+    n = 1
+    
+print(greet(sys.argv[1], n))
